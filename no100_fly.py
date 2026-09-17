7acde7a58457ab645fb384ab24f2c1c280609f15
ibovibo <ibrahim_enez25@trabzon.edu.tr>
2026-09-17 22:20:11 +0300

    100lu sistem

--- no100_fly.py
+++ no100_fly.py
@@ -1,29 +1,43 @@
 import random
+import numpy as np
 
 sol_aci = [6,5,4,3,3,3,2,2,1,1]
 sag_aci = [20]
 
-sol_puan = 10
-sag_puan = 10
-ogrenme_hizi = 0.3
-
-sinek_tercih = []
 
 
-for secim in range(100):
+def sinek():
 
+    sol_puan = 10
+    sag_puan = 10
+    ogrenme_hizi = random.uniform(0.05, 0.6)
+    sinek_tercih = []
+
+    
+    for cagir in range(100):
         
         sag_oran = sol_puan / (sol_puan + sag_puan)
         rs = random.random()
-        
+            
         if sag_oran > rs :
             sinek_tercih.append(0)
             sag_puan = sag_puan + ogrenme_hizi * (sum(sag_aci) - sag_puan)
-            
+                
 
         else:
             sinek_tercih.append(1)
             sol_puan = sol_puan + ogrenme_hizi * (sum(sol_aci) - sol_puan)
 
-print(f"ayrilik secimi = {sum(sinek_tercih)} elektirik secimi {100 - sum(sinek_tercih)}")
-print(sol_puan,sag_puan,sag_oran)
+    return sinek_tercih
+
+
+tum_sinekler = []
+
+for i in range(100):
+    tum_sinekler.append(sinek())
+   
+
+
+tablo = np.array(tum_sinekler)
+print(tablo.shape)
+print(tablo.mean(axis = 0))
